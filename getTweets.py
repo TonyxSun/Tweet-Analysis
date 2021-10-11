4baec1745a497e933ad59f61338c6a903f77466c
Tony Sun <68176295+TonyxSun@users.noreply.github.com>
2021-10-11 13:12:38 -0700

    Create getTweets.py

---
+++ getTweets.py
@@ -0,0 +1,21 @@
+'''
+get tweets by text
+'''
+
+#Veronica (seperating it out to prevent merge conflicts, not sure if it will work tbh, but I think just having 1 file looks better than 6 files. 
+
+#Tony
+
+'''
+get tweets by hashtag
+'''
+#Veronica
+
+#Tony
+
+'''
+get tweets by user
+'''
+#Veronica
+
+#Tony
