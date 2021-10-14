564c28743126b6bbd196f4c1b61236cd853d02ee
verxia <76971655+verxia@users.noreply.github.com>
2021-10-14 19:17:02 -0400

    Search for huawei, tweets from US gov't

--- getTweets.py
+++ getTweets.py
@@ -1,9 +1,18 @@
 import twint
 
+"""
 '''
 get tweets by text
 '''
 #Veronica (seperating it out to prevent merge conflicts, not sure if it will work tbh, but I think just having 1 file looks better than 6 files. 
+c = twint.Config()
+c.Search = 'Huawei'       # topic
+c.Limit = 500      # number of Tweets to scrape
+c.Store_csv = True       # store tweets in a json file
+# c.Show_hashtags = True
+c.Output = "./output/Huawei.csv"     # path to json file
+
+twint.run.Search(c)
 
 #Tony
 c = twint.Config()
@@ -26,7 +35,16 @@ get tweets by hashtag
 '''
 get tweets by user
 '''
+"""
 #Veronica
+c = twint.Config()
+c.Username = 'WhiteHouse'       # username
+c.Limit = 50      # number of Tweets to scrape
+c.Store_csv = True       # store tweets in a csv file
+c.Show_hashtags = True 
+c.Output = "./output/cbcnewsbc.csv"    # path to json file
+
+twint.run.Search(c)
 
 #Tony
 c = twint.Config()
